3dd61cd5ccaeda2bb8a9c72dcf3d4269ab9a02db
Rabten Samphel <rabten@protonmail.com>
2021-11-30 19:46:10 -0500

    created navbar & mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import'./sass/main.scss'
+
+const btn = document.querySelector('.hamburger')
+
+
+btn.addEventListener('click', () => {
+    const menu = document.querySelector('.mobileMenu')
+    menu.style.display = 'grid'
+    menu.style.height = '25rem'
+})

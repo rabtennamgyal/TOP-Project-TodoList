58790415e53403764f68fcf4f43127b15fe5072b
Rabten Samphel <rabten@protonmail.com>
2021-12-01 01:52:57 -0500

    created animation for mobile menu

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ const btn = document.querySelector('.hamburger')
 
 btn.addEventListener('click', () => {
     const menu = document.querySelector('.mobileMenu')
-    menu.style.display = 'grid'
-    menu.style.height = '25rem'
+    const burger = document.querySelector('.burger')
+
+    if (burger.classList.contains('open')) {
+        burger.classList.remove('open')
+        menu.classList.add('fadeOut')
+        menu.classList.remove('fadeIn')
+        //menu.style.animation = 'fadeOut 0.7s ease-in-out .1s forwards'
+    } else {
+        burger.classList.add('open')
+        menu.classList.add('fadeIn')
+        menu.classList.remove('fadeOut')
+        //menu.style.animation = 'fadeIn 0.7s ease-in-out .2s forwards'
+    }
 })

f22e96e682f54253b148610122323ae3e7710d67
Rabten Samphel <rabten@protonmail.com>
2021-12-02 07:05:39 -0500

    created newlist factory function & dom elements for it

---
+++ src/Modules/TodoList.js
@@ -0,0 +1,5 @@
+function TodoList(name, desc, due, pri, check) {
+    return {name, desc, due, pri, check}
+}
+
+export default TodoList
--- src/Modules/UI.js
+++ src/Modules/UI.js
@@ -1,3 +1,5 @@
+import TodoList from "./TodoList"
+
 // This is for opening & closing the mobile menu
 const btn = document.querySelector('.hamburger')
 
@@ -17,6 +19,7 @@ btn.addEventListener('click', () => {
 })
 // 🎈
 
+
 // This is for opening & closing the input form
 const addList = document.getElementById('addList')
 
@@ -25,5 +28,57 @@ addList.addEventListener('click', () => {
     inputForm.style.display = 'grid'
     addList.style.display = 'none'
 })
-
 // 🎈
+
+
+// Test 
+const addBtn = document.getElementById('addBtn')
+const delBtn = document.getElementById('delBtn')
+
+
+addBtn.addEventListener('click', () => {
+    const input = document.getElementById('input')
+
+    if (input.value) {
+        const listContainer = document.querySelector('.listContainer')
+        listContainer.style.display = 'flex'
+
+        // 0. Creating Box to contains all the list related datas
+        const newListBox = document.createElement('div')
+        newListBox.classList.add('newBoxStyle')
+
+        // 1. Creating the list 
+        const newList = document.createElement('p')
+        newList.classList.add('newListStyle')
+        newList.textContent = input.value
+
+        // 1. Creating the list description
+        const newDesc = document.createElement('div')
+        newDesc.classList.add('newDescStyle')
+
+
+        newListBox.appendChild(newList)
+        newListBox.appendChild(newDesc)
+
+        listContainer.appendChild(newListBox)
+
+        const inputForm = document.querySelector('.inputForm')
+        inputForm.style.display = 'none'
+        addList.style.display = 'block'
+
+        input.value = ''
+    } else {
+        alert('Add a new list')
+    }
+})
+
+delBtn.addEventListener('click', () => {
+    const inputForm = document.querySelector('.inputForm')
+    inputForm.style.display = 'none'
+    addList.style.display = 'block'
+
+    const listContainer = document.querySelector('.listContainer')
+    listContainer.style.display = 'none'
+})
+
+//
